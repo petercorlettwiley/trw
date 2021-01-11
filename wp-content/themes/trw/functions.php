85014aab3b0682774bb6bdb4c27a723cc4d326f7
Peter Wiley <peter.c.wiley@gmail.com>
2021-01-11 10:00:28 -0500

    dev & style: scrolling into view fx for homepage

--- wp-content/themes/trw/functions.php
+++ wp-content/themes/trw/functions.php
@@ -143,7 +143,7 @@ function trw_scripts() {
 	wp_enqueue_style( 'trw-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'trw-style', 'rtl', 'replace' );
 
-	//wp_enqueue_script( 'trw-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'trw-scrolling-fx', get_template_directory_uri() . '/js/scrolling-fx.js', array( 'jquery' ), _S_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'trw_scripts' );
 
---
+++ wp-content/themes/trw/js/scrolling-fx.js
@@ -0,0 +1,50 @@
+/* global jQuery */
+
+/**
+ * File scrolling-fx.js.
+ *
+ * @link https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling
+ */
+( function( $ ) {
+
+var $elements;
+
+if ( $('body').hasClass('home') && $('a.post-link-wrap').length > 0 ) {
+	// on home
+	$elements = $('a.post-link-wrap');
+} else {
+	return;
+}
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function testElements() {
+	$elements.each( function() {
+		if ( isScrolledIntoView($(this)) ) {
+			$(this).addClass('visible');
+		} else {
+			//$(this).removeClass('visible');
+		}
+	})
+}
+
+function init () {
+	testElements();
+	window.addEventListener('scroll', testElements, false);
+}
+
+if (document.readyState !== 'loading') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', init, false);
+}
+
+}( jQuery ) );
