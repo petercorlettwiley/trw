364b31aa4f116c88e30c77bd9765ae228c7d4f49
Peter Wiley <peter.c.wiley@gmail.com>
2021-02-06 20:37:13 -0500

    dev: about page functionality in plac

--- wp-content/themes/trw/js/about.js
+++ wp-content/themes/trw/js/about.js
@@ -9,17 +9,45 @@
 
 const $aboutLink = $('a[href="#about"');
 const $aboutPage = $('#about');
+const $aboutContent = $('#about a, #about .about-content');
 const $body = $('body');
 
-if ( $aboutLink.length == 0 || $aboutPage.length == 0 ) {
+if ( $aboutLink.length == 0 || $aboutPage.length == 0 || $aboutContent == 0 ) {
   return;
 }
 
-$aboutLink.click(function(event){
+$aboutLink.on('click', function (event){
   event.preventDefault();
 
+  openAboutPage();
+});
+
+$aboutContent.on('click', function (event) {
+  event.stopPropagation();
+});
+
+$aboutPage.on('click', function (event) {
+  event.preventDefault();
+
+  closeAboutPage();
+});
+
+$(document).keyup( function(event){
+  if (event.keyCode === 27) {
+    closeAboutPage();
+  }
+});
+
+function openAboutPage() {
   $aboutPage.toggleClass('open');
   $body.toggleClass('about-open');
-});
+}
+
+function closeAboutPage() {
+  if ($body.hasClass('about-open')) {
+    $body.removeClass('about-open');
+    $aboutPage.removeClass('open');
+  }
+}
 
 }( jQuery ) );
