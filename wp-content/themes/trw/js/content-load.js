03456648ab3646391a16f13c1e8ef80616c01fef
Peter Wiley <peter.c.wiley@gmail.com>
2021-07-14 20:20:32 -0400

    style: all pages and content load in and out smoothly

--- wp-content/themes/trw/js/content-load.js
+++ wp-content/themes/trw/js/content-load.js
@@ -8,6 +8,20 @@
 
   $(window).on('load', function(){
     $('body').addClass('loaded');
-  }); 
+  });
+
+  // clicking to new page and 'unloading' old page
+
+  var $links = $('a:not([href*="#"])');
+
+  $links.each(function(){
+    let href = $(this).attr('href');
+
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('body').removeClass('loaded');
+      setTimeout(function() { window.location.href = href; }, 300);
+    });
+  });
 
 }( jQuery ) );
