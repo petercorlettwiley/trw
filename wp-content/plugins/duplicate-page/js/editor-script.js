e7cd61cfd5461097c8b0d15a9727992334c013c6
Peter Wiley <peter.c.wiley@gmail.com>
2022-06-04 14:38:45 -0400

    dev: migrated content from staging stie

--- wp-content/plugins/duplicate-page/js/editor-script.js
+++ wp-content/plugins/duplicate-page/js/editor-script.js
@@ -4,7 +4,7 @@ var el = wp.element.createElement;
     var PluginPostStatusInfo = wp.editPost.PluginPostStatusInfo;
     var buttonControl = wp.components.Button;
 
-    function dpGutenButton({}) {
+    function MyPostStatusInfoPlugin({}) {
         return el(
             PluginPostStatusInfo,
             {
@@ -16,13 +16,12 @@ var el = wp.element.createElement;
                     isTertiary: true,
                     name: 'duplicate_page_link_guten',
                     isLink: true,
-                    title: dt_params.dp_post_title,
                     href : dt_params.dp_duplicate_link+"&post="+dt_params.dp_post_id+"&nonce="+dt_params.dtnonce
-                }, dt_params.dp_post_text
+                }, dt_params.dp_post_title
             )
         );
     }
 
     registerPlugin( 'dt-duplicate-post-status-info-plugin', {
-        render: dpGutenButton
+        render: MyPostStatusInfoPlugin
     } );
